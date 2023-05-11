acdd4928fb580cab39d5b2088ef615cc3d370f00
Dampilon <td282002@gmail.com>
2023-05-11 15:16:06 +0700

    Defined 3 factories (Cpp, C#, Java)

--- SoftWareDevTech_CodeGenerator/Classes/ClassFactories/JavaFactory.cpp
+++ SoftWareDevTech_CodeGenerator/Classes/ClassFactories/JavaFactory.cpp
@@ -1,1 +1,13 @@
 #include "JavaFactory.h"
+
+UnitPtr JavaFactory::createClass(const std::string &name) const {
+    return std::make_shared<JavaClassUnit>(name);
+}
+
+UnitPtr JavaFactory::createMethod(const std::string &name, const std::string &returnType, Unit::Flags flags) const {
+    return std::make_shared<JavaMethodUnit>(name, returnType, flags);
+}
+
+UnitPtr JavaFactory::createPrintOperator(const std::string &text) const {
+    return std::make_shared<JavaPrintOperatorUnit>(text);
+}
